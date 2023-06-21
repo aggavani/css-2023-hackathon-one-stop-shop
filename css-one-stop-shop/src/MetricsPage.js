34f23ab14dc70733f96085ae3d902973c5e1fe6f
Robert Huang <robehuan@amazon.com>
2023-06-21 12:14:36 -0700

    Add customer

--- css-one-stop-shop/src/MetricsPage.js
+++ css-one-stop-shop/src/MetricsPage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import materialConsistency from './data/SHIRT/material/metrics_consistency.png';
 import materialValidValues from './data/SHIRT/material/metrics_valid_values.png';
 import materialInvalidValues from './data/SHIRT/material/invalid_values.png';
+import customerValues from './data/SHIRT/material/customer_values.png';
 
 export const MetricsPage = () => {
   const formValues = localStorage.getItem("formValues");
@@ -14,6 +15,7 @@ export const MetricsPage = () => {
         <img src={materialConsistency} alt='Material Consistency'/>
         <img src={materialValidValues} alt='Material Valid Values'/>
         <img src={materialInvalidValues} alt='Material Valid Values'/>
+        <img src={customerValues} alt='Material Customer Values'/>
       </div>
     );
   }
